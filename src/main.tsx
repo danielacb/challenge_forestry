216d534ea349b6c01a1638c3bdcdc42792032173
Daniela C. Barbosa <contato@danielacb.com>
2023-08-15 02:50:16 -0300

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Hotjar from '@hotjar/browser'
 import App from './App.tsx'
 import './styles/globals.scss'
+
+Hotjar.init(3613833, 6)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
